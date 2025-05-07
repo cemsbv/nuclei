97de4531269f25f3947d82eb5030004948cbaf30
Robin Wimmers <r.wimmers@cemsbv.io>
2025-05-07 13:56:19 +0200

    style(super-linter): format files with super-linter

--- run_super_linter.sh
+++ run_super_linter.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 docker run \
---env VALIDATE_ALL_CODEBASE=false \
---env RUN_LOCAL=true \
---env DEFAULT_BRANCH=main \
---env FIX_PYTHON_ISORT=true \
---env FIX_PYTHON_BLACK=true \
---env FIX_YAML_PRETTIER=true \
---env VALIDATE_JSCPD=false \
---env VALIDATE_CSS=false \
---env VALIDATE_BASH=false \
---env VALIDATE_YAML=false \
---env VALIDATE_PYTHON_PYLINT=false \
---env VALIDATE_NATURAL_LANGUAGE=false \
---env VALIDATE_MARKDOWN=false \
---env LINTER_RULES_PATH=/ \
---env PYTHON_BLACK_CONFIG_FILE=pyproject.toml \
---env PYTHON_ISORT_CONFIG_FILE=pyproject.toml \
---env PYTHON_MYPY_CONFIG_FILE=pyproject.toml \
---env PYTHON_FLAKE8_CONFIG_FILE=.flake8 \
--v $(pwd):/tmp/lint ghcr.io/super-linter/super-linter:v7
+	--env VALIDATE_ALL_CODEBASE=false \
+	--env RUN_LOCAL=true \
+	--env DEFAULT_BRANCH=main \
+	--env FIX_PYTHON_ISORT=true \
+	--env FIX_PYTHON_BLACK=true \
+	--env FIX_YAML_PRETTIER=true \
+	--env FIX_SHELL_SHFMT=true \
+	--env VALIDATE_JSCPD=false \
+	--env VALIDATE_CSS=false \
+	--env VALIDATE_BASH=false \
+	--env VALIDATE_YAML=false \
+	--env VALIDATE_PYTHON_PYLINT=false \
+	--env VALIDATE_NATURAL_LANGUAGE=false \
+	--env VALIDATE_MARKDOWN=false \
+	--env LINTER_RULES_PATH=/ \
+	--env PYTHON_BLACK_CONFIG_FILE=pyproject.toml \
+	--env PYTHON_ISORT_CONFIG_FILE=pyproject.toml \
+	--env PYTHON_MYPY_CONFIG_FILE=pyproject.toml \
+	--env PYTHON_FLAKE8_CONFIG_FILE=.flake8 \
+	-v $(pwd):/tmp/lint ghcr.io/super-linter/super-linter:v7
