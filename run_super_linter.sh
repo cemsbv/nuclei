b20a888a99c6d4b141553eb08745db25ad4cb2ca
Robin Wimmers <r.wimmers@cemsbv.io>
2025-05-07 13:47:09 +0200

    style(super-linter): format yaml file with prettier

--- run_super_linter.sh
+++ run_super_linter.sh
@@ -3,6 +3,10 @@
 docker run \
 --env VALIDATE_ALL_CODEBASE=false \
 --env RUN_LOCAL=true \
+--env DEFAULT_BRANCH=main \
+--env FIX_PYTHON_ISORT=true \
+--env FIX_PYTHON_BLACK=true \
+--env FIX_YAML_PRETTIER=true \
 --env VALIDATE_JSCPD=false \
 --env VALIDATE_CSS=false \
 --env VALIDATE_BASH=false \
@@ -15,4 +19,4 @@ docker run \
 --env PYTHON_ISORT_CONFIG_FILE=pyproject.toml \
 --env PYTHON_MYPY_CONFIG_FILE=pyproject.toml \
 --env PYTHON_FLAKE8_CONFIG_FILE=.flake8 \
--v $(pwd):/tmp/lint github/super-linter:v4
+-v $(pwd):/tmp/lint ghcr.io/super-linter/super-linter:v7
